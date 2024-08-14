863989c6d9add249d6cca52b572a9062b50f724e
José Alarcón <64882085+joseq3832@users.noreply.github.com>
2024-08-14 17:20:10 -0500

    wip feature

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -31,7 +31,7 @@ export const Home = () => {
           <div>
             <div className="mb-16">
               <H1 className="mb-6">
-                Todo en developppp wipp <br /> solo <span className="text-rick-primary">Lugar.</span>
+                Feature WIP <br /> solo <span className="text-rick-primary">Lugar.</span>
               </H1>
               <H4>Personajes, lugares, episodios y mucho más.</H4>
             </div>
